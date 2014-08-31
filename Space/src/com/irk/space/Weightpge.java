b3ff89908dbc1b26765f2d8325d0dd0b075adb2a
isururk <isururk@gmail.com>
2014-09-01 00:27:13 +0530

    home develop 1

--- Space/src/com/irk/space/Weightpge.java
+++ Space/src/com/irk/space/Weightpge.java
@@ -1,7 +1,13 @@
 package com.irk.space;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TabHost;
@@ -9,10 +15,12 @@ import android.widget.TabHost.TabSpec;
 
 public class Weightpge extends Activity{
 
+	final Context context = this;
 	TabHost th;
 	TabSpec specs;
 	Button sun, mercury, venus, moon, mars, jupitor, saturn, uranus, neptune;
 	EditText weight;
+	int data=0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -43,6 +51,35 @@ public class Weightpge extends Activity{
 		neptune = (Button)findViewById(R.id.btnneptune);
 		
 		weight = (EditText)findViewById(R.id.txtweight);
+		
+		sun.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data = Integer.parseInt(weight.getText().toString())*2;
+				AlertDialog.Builder popup = new AlertDialog.Builder(context);
+				popup.setTitle("Your Weight");
+				if (weight.getText().equals("")) {
+					popup.setMessage("Please enter your weight in 1st tab");
+				}
+				else {
+					popup.setMessage("In the Sun your weight = "+data+"Kg");
+				}
+				
+				popup.setCancelable(false)
+				.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                            // if yes button is clicked, close current activity
+                    	//AlertDialog.this.finish();
+                    }
+              });
+          // create alert dialog
+          AlertDialog alertDialog = popup.create();
+          // Display alert dialog 
+          alertDialog.show();
+			}
+		});
 	}
 
 }
