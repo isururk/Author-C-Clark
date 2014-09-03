cfa4ccd45681580a71cb3e6c8617acedb3163667
isururk <isururk@gmail.com>
2014-09-04 01:12:20 +0530

    home develop 2

--- Space/src/com/irk/space/Weightpge.java
+++ Space/src/com/irk/space/Weightpge.java
@@ -13,20 +13,21 @@ import android.widget.EditText;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
-public class Weightpge extends Activity{
+public class Weightpge extends Activity {
 
 	final Context context = this;
 	TabHost th;
 	TabSpec specs;
 	Button sun, mercury, venus, moon, mars, jupitor, saturn, uranus, neptune;
 	EditText weight;
-	int data=0;
+	double data = 0;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.weightpge);
-		
+
 		th = (TabHost) findViewById(R.id.tabhost);
 
 		th.setup();
@@ -39,45 +40,233 @@ public class Weightpge extends Activity{
 		specs.setContent(R.id.Planet);
 		specs.setIndicator("Planet");
 		th.addTab(specs);
-		
-		sun = (Button)findViewById(R.id.btnsun);
-		mercury = (Button)findViewById(R.id.btnmercury);
-		venus = (Button)findViewById(R.id.btnvenus);
-		moon = (Button)findViewById(R.id.btnmoon);
-		mars = (Button)findViewById(R.id.btnmars);
-		jupitor = (Button)findViewById(R.id.btnjupitor);
-		saturn = (Button)findViewById(R.id.btnsaturn);
-		uranus = (Button)findViewById(R.id.btnuranus);
-		neptune = (Button)findViewById(R.id.btnneptune);
-		
-		weight = (EditText)findViewById(R.id.txtweight);
-		
+
+		sun = (Button) findViewById(R.id.btnsun);
+		mercury = (Button) findViewById(R.id.btnmercury);
+		venus = (Button) findViewById(R.id.btnvenus);
+		moon = (Button) findViewById(R.id.btnmoon);
+		mars = (Button) findViewById(R.id.btnmars);
+		jupitor = (Button) findViewById(R.id.btnjupitor);
+		saturn = (Button) findViewById(R.id.btnsaturn);
+		uranus = (Button) findViewById(R.id.btnuranus);
+		neptune = (Button) findViewById(R.id.btnneptune);
+
+		weight = (EditText) findViewById(R.id.txtweight);
+
 		sun.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data = Double.parseDouble(weight.getText().toString())*2;
+				AlertDialog.Builder popup = new AlertDialog.Builder(context);
+				popup.setTitle("Your Weight");
+				popup.setMessage("In the Sun your weight = " + data + "Kg");
+				popup.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if yes button is clicked, close current
+								// activity
+								// AlertDialog.this.finish();
+							}
+						});
+				// create alert dialog
+				AlertDialog alertDialog = popup.create();
+				// Display alert dialog
+				alertDialog.show();
+
+			}
+		});
+		
+		mercury.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				data = Integer.parseInt(weight.getText().toString())*2;
+				data = Double.parseDouble(weight.getText().toString())*3;
 				AlertDialog.Builder popup = new AlertDialog.Builder(context);
 				popup.setTitle("Your Weight");
-				if (weight.getText().equals("")) {
-					popup.setMessage("Please enter your weight in 1st tab");
-				}
-				else {
-					popup.setMessage("In the Sun your weight = "+data+"Kg");
-				}
-				
-				popup.setCancelable(false)
-				.setPositiveButton("OK",new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                            // if yes button is clicked, close current activity
-                    	//AlertDialog.this.finish();
-                    }
-              });
-          // create alert dialog
-          AlertDialog alertDialog = popup.create();
-          // Display alert dialog 
-          alertDialog.show();
+				popup.setMessage("In the Sun your weight = " + data + "Kg");
+				popup.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if yes button is clicked, close current
+								// activity
+								// AlertDialog.this.finish();
+							}
+						});
+				// create alert dialog
+				AlertDialog alertDialog = popup.create();
+				// Display alert dialog
+				alertDialog.show();
+			}
+		});
+		
+		venus.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data = Double.parseDouble(weight.getText().toString())*4.5;
+				AlertDialog.Builder popup = new AlertDialog.Builder(context);
+				popup.setTitle("Your Weight");
+				popup.setMessage("In the Sun your weight = " + data + "Kg");
+				popup.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if yes button is clicked, close current
+								// activity
+								// AlertDialog.this.finish();
+							}
+						});
+				// create alert dialog
+				AlertDialog alertDialog = popup.create();
+				// Display alert dialog
+				alertDialog.show();
+			}
+		});
+		
+		moon.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data = Double.parseDouble(weight.getText().toString())*5;
+				AlertDialog.Builder popup = new AlertDialog.Builder(context);
+				popup.setTitle("Your Weight");
+				popup.setMessage("In the Sun your weight = " + data + "Kg");
+				popup.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if yes button is clicked, close current
+								// activity
+								// AlertDialog.this.finish();
+							}
+						});
+				// create alert dialog
+				AlertDialog alertDialog = popup.create();
+				// Display alert dialog
+				alertDialog.show();
+			}
+		});
+		
+		mars.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data = Double.parseDouble(weight.getText().toString())*7;
+				AlertDialog.Builder popup = new AlertDialog.Builder(context);
+				popup.setTitle("Your Weight");
+				popup.setMessage("In the Sun your weight = " + data + "Kg");
+				popup.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if yes button is clicked, close current
+								// activity
+								// AlertDialog.this.finish();
+							}
+						});
+				// create alert dialog
+				AlertDialog alertDialog = popup.create();
+				// Display alert dialog
+				alertDialog.show();
+			}
+		});
+		
+		jupitor.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data = Double.parseDouble(weight.getText().toString())*6.8;
+				AlertDialog.Builder popup = new AlertDialog.Builder(context);
+				popup.setTitle("Your Weight");
+				popup.setMessage("In the Sun your weight = " + data + "Kg");
+				popup.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if yes button is clicked, close current
+								// activity
+								// AlertDialog.this.finish();
+							}
+						});
+				// create alert dialog
+				AlertDialog alertDialog = popup.create();
+				// Display alert dialog
+				alertDialog.show();
+			}
+		});
+		
+		saturn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data = Double.parseDouble(weight.getText().toString())*4.65;
+				AlertDialog.Builder popup = new AlertDialog.Builder(context);
+				popup.setTitle("Your Weight");
+				popup.setMessage("In the Sun your weight = " + data + "Kg");
+				popup.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if yes button is clicked, close current
+								// activity
+								// AlertDialog.this.finish();
+							}
+						});
+				// create alert dialog
+				AlertDialog alertDialog = popup.create();
+				// Display alert dialog
+				alertDialog.show();
+			}
+		});
+		
+		uranus.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data = Double.parseDouble(weight.getText().toString())*2.3;
+				AlertDialog.Builder popup = new AlertDialog.Builder(context);
+				popup.setTitle("Your Weight");
+				popup.setMessage("In the Sun your weight = " + data + "Kg");
+				popup.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if yes button is clicked, close current
+								// activity
+								// AlertDialog.this.finish();
+							}
+						});
+				// create alert dialog
+				AlertDialog alertDialog = popup.create();
+				// Display alert dialog
+				alertDialog.show();
+			}
+		});
+		
+		neptune.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data = Double.parseDouble(weight.getText().toString())*2.65;
+				AlertDialog.Builder popup = new AlertDialog.Builder(context);
+				popup.setTitle("Your Weight");
+				popup.setMessage("In the Sun your weight = " + data + "Kg");
+				popup.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// if yes button is clicked, close current
+								// activity
+								// AlertDialog.this.finish();
+							}
+						});
+				// create alert dialog
+				AlertDialog alertDialog = popup.create();
+				// Display alert dialog
+				alertDialog.show();
 			}
 		});
 	}
